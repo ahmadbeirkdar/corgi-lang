937f15121f5ba6c725f329d90f0e5506bc15ab72
ahmadbeirkdar <ahmadb@protonmail.com>
2021-02-23 05:56:56 -0500

    token class constructor and methods fixed

--- src/token.h
+++ src/token.h
@@ -5,28 +5,36 @@
 #ifndef CORGI_LANG_TOKEN_H
 #define CORGI_LANG_TOKEN_H
 #include <any>
+#include <typeindex>
 
 enum Kind{
-    EndOfFile,Def,Extern,Identifier,Nil
+    EndOfFile,Def,Extern,Identifier,Nil,
     Str,Bool,Char,Array,
     U8,U16,U32,U64,
     I8,I16,I32,I64,
     F32,F64,
 };
 
-template<typename T = void*>
+
 class token {
 public:
 
-    token()=default;
+    token()=delete;
+
+    token(Kind kind, uint32_t line = 0, uint32_t column = 0)
+            : kind(kind), location(line,column), type(typeid(nullptr)) {}
+
+    template<typename T>
     token(Kind kind, T value, uint32_t line = 0, uint32_t column = 0)
-        : kind(kind), value(std::make_any<T>(value)), location(line,column) {}
+        : kind(kind), value(std::make_any<T>(value)), type(typeid(T)),location(line,column) {}
 
-    auto get_value()  -> T          {return value.has_value() ? std::any_cast<T>(value) : std::any();}
-    auto get_kind()   -> Kind       {return kind;}
-    auto get_line()   -> uint32_t   {return location.first;}
-    auto get_column() -> uint32_t   {return location.second;}
+    template<typename T>
+    [[nodiscard]] auto get_value()  const  -> auto       {return std::any_cast<T>(value);}
+    [[nodiscard]] auto get_kind()   const  -> Kind       {return kind;}
+    [[nodiscard]] auto get_line()   const  -> uint32_t   {return location.first;}
+    [[nodiscard]] auto get_column() const  -> uint32_t   {return location.second;}
 
+    std::type_index type;
 private:
     std::any value{};
     std::pair<uint32_t,uint32_t> location{0,0}; // <line,column>

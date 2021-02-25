f1a671c52a81bd984cdde1de6c5556e74d792c60
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-02-25 17:28:07 -0500

    AST

---
+++ AST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ahmad Beirkdar on 2021-02-25.
+//
+
+#include "AST.h"
---
+++ AST.h
@@ -0,0 +1,14 @@
+//
+// Created by Ahmad Beirkdar on 2021-02-25.
+//
+
+#ifndef CORGI_LANG_AST_H
+#define CORGI_LANG_AST_H
+
+
+class AST {
+
+};
+
+
+#endif //CORGI_LANG_AST_H
